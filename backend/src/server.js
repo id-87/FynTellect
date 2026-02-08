533e00a74b766dfd79d7dda281aaa3bf8f1df878
[Harshit Gupta] <hrstgpt87@gmail.com>
2026-02-08 19:14:15 +0530

    Root path

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,6 +3,11 @@ let app=express()
 app.get('/health',(req,res)=>{
     res.send("Healthy")
 })
+
+app.get('/',(req,res)=>{
+    res.send("Welcome to fintellect")
+})
+
 app.listen(3000,()=>{
     console.log("Server is running")
 })
