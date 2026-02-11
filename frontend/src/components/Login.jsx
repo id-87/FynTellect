b0dc154d1d968d788ca0d2dfe12499d89bbb7b69
[Harshit Gupta] <hrstgpt87@gmail.com>
2026-02-11 22:41:31 +0530

    Login and home

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -3,6 +3,17 @@ import React from 'react'
 const Login = () => {
   return (
     <div>
+      <form >
+        <label >
+          Username
+          <input type="text" />
+        </label>
+
+        <label >
+          Password
+          <input type="text" />
+        </label>
+      </form>
       
     </div>
   )
