848d678a45f6c0946a4da3c101ea84a9a3506636
[Harshit Gupta] <hrstgpt87@gmail.com>
2026-02-12 16:41:03 +0530

    Alert model definition

--- backend/src/models/alertModel.js
+++ backend/src/models/alertModel.js
@@ -1,11 +1,32 @@
 const mongoose=require('mongoose')
 const model=new mongoose.Schema({
-    _id:{},
-    userId:{},
-    type:{},
-    message:{},
-    relatedTransactionID:{},
-    isRead:{},
-    createdAt:{}
+    _id:{
+        type:Number,
+        require:true
+    },
+    userId:{
+        type:Number,
+        require:true
+    },
+    type:{
+        type:String,
+        require:true
+    },
+    message:{
+        type:String,
+        require:true
+    },
+    relatedTransactionID:{
+        type:Number,
+        require:true
+    },
+    isRead:{
+        type:Boolean,
+        require:true
+    },
+    createdAt:{
+        type:Number,
+        require:true
+    }
 
 })
