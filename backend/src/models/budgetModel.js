8a1e406c97b0b59323ce8736d066e7b3cdd59032
[Harshit Gupta] <hrstgpt87@gmail.com>
2026-02-12 16:44:06 +0530

    Budget model defninition

--- backend/src/models/budgetModel.js
+++ backend/src/models/budgetModel.js
@@ -1,10 +1,29 @@
 
 const mongoose=require('mongoose')
 const model=new mongoose.Schema({
-    _id:{},
-    userID:{},
-    category:{},
-    monthlyLimit:{},
-    createdAt:{},
-    updatedAt:{}
+    _id:{
+        type:Number,
+        require:true
+    },
+    userID:{
+        type:Number,
+        require:true,
+
+    },
+    category:{
+        type:String,
+        require:true
+    },
+    monthlyLimit:{
+        type:Number,
+        require:true
+    },
+    createdAt:{
+        type:Number,
+        require:true
+    },
+    updatedAt:{
+        type:Number,
+        require:true
+    }
 })
