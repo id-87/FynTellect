56ac44e312a07850d8abbb6ab44ca6a28a76d2e7
[Harshit Gupta] <hrstgpt87@gmail.com>
2026-02-12 16:46:04 +0530

    userModel definition

--- backend/src/models/userModel.js
+++ backend/src/models/userModel.js
@@ -1,9 +1,27 @@
 const mongoose=require('mongoose')
 const model=new mongoose.Schema({
-    _id:{},
-    name:{},
-    username:{},
-    hashedPassword:{},
-    role:{},
-    createdAt:{}
+    _id:{
+        type:Number,
+        require:true
+    },
+    name:{
+        type:String,
+        require:true
+    },
+    username:{
+        type:String,
+        require:true
+    },
+    hashedPassword:{
+        type:String,
+        require:true
+    },
+    role:{
+        type:String,
+        require:true
+    },
+    createdAt:{
+        type:Number,
+        require:true
+    }
 })
