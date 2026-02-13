b893b358ea268cf4e08c92392a2c8ec80304dc30
[Harshit Gupta] <hrstgpt87@gmail.com>
2026-02-13 18:25:18 +0530

    Transaction Model

--- backend/src/models/transactionModel.js
+++ backend/src/models/transactionModel.js
@@ -1,12 +1,36 @@
 const mongoose=require('mongoose')
 const model=new mongoose.Schema({
-    _id:{},
-        userID:{},
-        type:{},
-        amount:{},
-        category:{},
-        status:{},
+    _id:{
+        type:Number,
+        required:true
+    },
+        userID:{
+            type:Number,
+            required:true,
+        },
+        type:{
+            type:String,
+            required:true
+        },
+        amount:{
+            type:Number,
+            required:true
+        },
+        category:{
+            type:String,
+            required:true
+        },
+        status:{
+            type:String,
+            required:true
+        },
         // fraudScore(to be added later)
-        createdAt:{},
-        updatedAt:{}
+        createdAt:{
+            type:Number,
+            required:true
+        },
+        updatedAt:{
+            type:Number,
+            required:true
+        }
 })
