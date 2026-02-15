9da7cd46003189dfa92e098c7f7aa7de055a0d07
[Harshit Gupta] <hrstgpt87@gmail.com>
2026-02-15 20:45:27 +0530

    Signup functionality

--- frontend/src/components/Signup.jsx
+++ frontend/src/components/Signup.jsx
@@ -1,8 +1,61 @@
 import React from 'react'
+import { useState } from 'react'
+import axios from 'axios'
 
 const Signup = () => {
+  const [name,setName]=useState("")
+  const [username,setusername]=useState("")
+  const [password,setPassword]=useState("")
+  const [confirmPass,setConfirmPass]=useState("")
+  const [role,setRole]=useState("")
+
+  const handleSubmit=async()=>{
+
+  }
   return (
     <div>
+      <form onSubmit={handleSubmit}>
+        <label >
+          Full Name
+          <input type="text"
+          value={name}
+          onChange={(e)=>setName(e.target.value)
+          }
+           />
+        </label>
+        <label >
+          Username
+          <input type="text"
+          value={username}
+          onChange={(e)=>setusername(e.target.value)
+          }
+           />
+        </label>
+        <label >
+          Password
+          <input type="text"
+          value={password}
+          onChange={(e)=>setPassword(e.target.value)
+          }
+           />
+        </label>
+        <label >
+          Confirm Password
+          <input type="text"
+          value={confirmPass}
+          onChange={(e)=>setConfirmPass(e.target.value)
+          }
+           />
+        </label>
+        <label >
+          Role
+          <input type="text"
+          value={role}
+          onChange={(e)=>setRole(e.target.value)
+          }
+           />
+        </label>
+      </form>
       
     </div>
   )
