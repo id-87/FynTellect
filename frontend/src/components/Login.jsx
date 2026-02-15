156a522748b944aa1a615c5e4294cecb7e8fc4f8
[Harshit Gupta] <hrstgpt87@gmail.com>
2026-02-15 20:36:51 +0530

    Log form

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,17 +1,29 @@
 import React from 'react'
+import { useState } from 'react'
 
 const Login = () => {
+  const [userName,setUserName]=useState("")
+  const [password,setPassword]=useState("")
+
+  const handleSubmit=()=>{
+
+  }
   return (
+
     <div>
       <form >
         <label >
           Username
-          <input type="text" />
+          <input type="text"
+          value={userName}
+          onChange={(e)=>setUserName(e.target.value)} />
         </label>
 
         <label >
           Password
-          <input type="text" />
+          <input type="text"
+          value={password}
+          onChange={(e)=>setPassword(e.target.value)} />
         </label>
       </form>
       
