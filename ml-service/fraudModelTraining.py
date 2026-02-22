078b7606bbcb3218a7cff35a75a42b1ce39a9f7a
[Harshit Gupta] <hrstgpt87@gmail.com>
2026-02-23 00:50:13 +0530

    Fraud Model Evaluation metrics

--- ml-service/fraudModelTraining.py
+++ ml-service/fraudModelTraining.py
@@ -3,6 +3,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 from sklearn.ensemble import RandomForestClassifier
 import joblib
+from sklearn.metrics import classification_report, confusion_matrix, roc_auc_score
 
 df=pd.read_csv('fraudModelTrain.csv')
 # encoders={}
@@ -25,9 +26,25 @@ X=pd.get_dummies(X,drop_first=True)
 y=df['Is Fraudulent']
 X_train,X_test,y_train,y_test=train_test_split(X,y,random_state=42,test_size=0.2)
 
-model=RandomForestClassifier()
+model=RandomForestClassifier(class_weight='balanced',random_state=42)
 model.fit(X_train,y_train)
-accuracy_score=model.score(X_test,y_test)
-print(accuracy_score)
+
+
+
+
+
+y_pred = model.predict(X_test)
+y_prob = model.predict_proba(X_test)[:, 1]
+
+print("Confusion Matrix:")
+print(confusion_matrix(y_test, y_pred))
+
+print("\nClassification Report:")
+print(classification_report(y_test, y_pred))
+
+print("\nROC-AUC Score:", roc_auc_score(y_test, y_prob))
+
+
+
 joblib.dump(model,'fraudModel.pkl')
 joblib.dump(X.columns.tolist(), "fraud_columns.pkl")
