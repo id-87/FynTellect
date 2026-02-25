3a045c24e814804abdf7abb3014013fecc0906e0
[Harshit Gupta] <hrstgpt87@gmail.com>
2026-02-25 21:56:09 +0530

    Setting values in forms of transaction form

--- frontend/src/components/Transactions.jsx
+++ frontend/src/components/Transactions.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
-
+import { useState } from 'react'
 const Transactions = () => {
     const handleSubmit=()=>{
-
+        const [amount,setAmount]=useState(0)
+        const [type,setType]=useState("")
+        const [category,setCategory]=useState("")
     }
   return (
     <div>
@@ -12,13 +14,18 @@ const Transactions = () => {
             </label>
             <label>Type: 
                 
-                <select >
+                <select value={type} onChange={(e)=>setType(e.target.value)}>
                     <option value="debit">Debit</option>
                     <option value="credit">Credit</option>
                 </select>
             </label>
             <label>Category: 
-                <input type="number" />
+                <select value={category} onChange={(e)=>setCategory(e.target.value)}>
+                    <option value="marketing">Marketing</option>
+                    <option value="development">Development</option>
+                    <option value="testing">Testing</option>
+                    <option value="legal">Legal</option>
+                </select>
             </label>
         </form>
       
