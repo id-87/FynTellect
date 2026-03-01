0543af79fa7dc6608b45a1d5c9653292f365368a
[Harshit Gupta] <hrstgpt87@gmail.com>
2026-03-01 17:17:49 +0530

    Importing components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,11 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import {Router,Routes,Route} from 'react-router-dom'
-
+import Login from './components/Login'
+import Signup from './components/Signup'
+import Navbar from './components/Navbar'
+import Transactions from './components/Transactions'
+import Home from './components/Home'
 function App() {
   const [count, setCount] = useState(0)
 
