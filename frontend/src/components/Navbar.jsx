5c0d3b76c4ab012b7122d0816510cb823940e885
[Harshit Gupta] <hrstgpt87@gmail.com>
2026-03-01 17:30:51 +0530

    Links on Navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
     <div>
       <ul>
-        <li>Home</li>
-        <li>Transactions</li>
-        <li>Login</li>
-        <li>Signup</li>
-        
+        <li><Link to='/home'>Home</Link></li>
+        <li><Link to='/transactions'>Transactions</Link></li>
+        <li><Link to='/config'>Configuration</Link></li>
+        <li><Link to='/alert'>Alerts</Link></li>
+        <li><Link to='/login'>Login</Link></li>
+        <li><Link to='/signup'>Signup</Link></li>
+
       </ul>
     </div>
   )
