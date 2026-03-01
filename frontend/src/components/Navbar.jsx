e2479d825e12dee93c6273e9865533120ef3c67f
[Harshit Gupta] <hrstgpt87@gmail.com>
2026-03-01 17:16:08 +0530

    Initiating routes

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -3,7 +3,13 @@ import React from 'react'
 const Navbar = () => {
   return (
     <div>
-      
+      <ul>
+        <li>Home</li>
+        <li>Transactions</li>
+        <li>Login</li>
+        <li>Signup</li>
+        
+      </ul>
     </div>
   )
 }
