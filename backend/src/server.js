b229a5d37b907f48dc4b788c2901b7a3a8b765c7
[Harshit Gupta] <hrstgpt87@gmail.com>
2026-03-02 17:23:32 +0530

    OOPs implementation in Server

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,24 +1,85 @@
 const express=require('express')
 const app=express()
-app.use(express.json())
-require('dotenv').config()
+// app.use(express.json())
+// require('dotenv').config()
 const authRoutes = require('./routes/authRoutes')
 const transactionRoutes=require('./routes/transactionRoutes')
 const connectDB = require('./config/db.config')
+
+
 const PORT = process.env.PORT || 4000
 const cors=require('cors')
-app.use(cors())
 
-connectDB()
+// app.use(cors())
 
-app.get('/health',(req,res)=>{
-    res.send("Server running healthy")
+// connectDB()
+
+// app.get('/health',(req,res)=>{
+//     res.send("Server running healthy")
+// })
+
+
+// app.use('/auth',authRoutes)
+// app.use('/transaction',transactionRoutes)
+
+// app.listen(PORT,()=>{
+//     console.log(`Server is running on port ${PORT}`)
+// })
+
+
+
+class Server{
+    express
+    app
+    authRoutes
+    transactionRoutes
+    port
+    cors
+
+    cb(){
+        console.log(`Server is running on ${this.port}`)
+    }
+
+
+    healthRoute(){
+        this.app.get('/health',(req,res)=>{
+            res.send("Server running healthy")
 })
 
+    }
 
-app.use('/auth',authRoutes)
-app.use('/transaction',transactionRoutes)
+    authRoute(){
+        this.app.use('/auth',this.authRoutes)
+    }
 
-app.listen(PORT,()=>{
-    console.log(`Server is running on port ${PORT}`)
-})
+    transactionRoute(){
+        this.app.use('/transaction',this.transactionRoutes)
+    }
+
+
+    startServer(){
+        this.app.listen(this.port,this.cb())
+    }
+
+    
+    constructor(exp,authR,transR,cors,port){
+        require('dotenv').config()
+        connectDB()
+
+        this.express=exp
+        this.cors=cors
+        this.app=this.express()
+        this.app.use(this.express.json())
+        this.port=port
+        this.authRoutes=authR
+        this.transactionRoutes=transR
+        this.app.use(this.cors())
+    }
+
+}
+
+const server=new Server(express,authRoutes,transactionRoutes,cors,PORT)
+server.startServer()
+server.healthRoute()
+server.authRoute()
+server.transactionRoute()
