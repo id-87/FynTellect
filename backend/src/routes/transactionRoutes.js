f44f94a78649c3322783982b9eedf034a9a2b459
[Harshit Gupta] <hrstgpt87@gmail.com>
2026-03-02 17:38:07 +0530

    Transaction routes oops implementation

--- backend/src/routes/transactionRoutes.js
+++ backend/src/routes/transactionRoutes.js
@@ -1,5 +1,20 @@
 const express=require('express')
 const {postTransaction}=require('..//services/transactionService')
 const router=express.Router()
-router.post('/post',postTransaction)
-module.exports=router
+// router.post('/post',postTransaction)
+// module.exports=router
+
+
+class TransactionRoutes{
+    router
+    postTransaction
+
+    constructor(router,postTransaction){
+        this.router=router
+        this.postTransaction=postTransaction
+        this.router.post('/post',this.postTransaction)
+    }
+}
+
+const transR=new TransactionRoutes(router,postTransaction)
+module.exports=transR.router
