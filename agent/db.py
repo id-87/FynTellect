06953c0eab46e9bf1e2ccec1f40432b75dd34ed4
[Harshit Gupta] <hrstgpt87@gmail.com>
2026-03-18 22:19:09 +0530

    Fetch transaction

--- agent/db.py
+++ agent/db.py
@@ -10,3 +10,9 @@ client = MongoClient(os.getenv("MONGODB_URI"))
 db=client['Fin']
 
 trans_col=db["transactions"]
+
+def get_transaction(user_id:str):
+    user_id=ObjectId(user_id)
+    transactions=list(trans_col[{"user":user_id}])
+    return transactions
+
