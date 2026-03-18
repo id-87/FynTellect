19ddfb21fe2065729b072baa4bf1473089b8ae22
[Harshit Gupta] <hrstgpt87@gmail.com>
2026-03-18 22:32:27 +0530

    bugs fixing

--- agent/db.py
+++ agent/db.py
@@ -25,6 +25,9 @@ def get_transaction_by_category(user_id:str,category:str):
 
 def get_monthly_transaction(user_id:str,month:int,year:int):
     start_date=datetime(year,month,1)
-    end_date=datetime(year,month+1,1)
-    transactions=list(trans_col.find({"createdAt":{"$gte":start_date,"$lte":end_date}}))
+    if(month==12):
+        end_date=datetime(year+1,1,1)
+    else:
+        end_date=datetime(year,month+1,1)
+    transactions=list(trans_col.find({"user":user_id,"createdAt":{"$gte":start_date,"$lt":end_date}}))
     return transactions
