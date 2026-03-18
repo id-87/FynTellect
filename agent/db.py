f71c4f14ca2ffccc0fbb8d4ae35fe2fe04d43293
[Harshit Gupta] <hrstgpt87@gmail.com>
2026-03-18 22:29:36 +0530

    Monthly transactions

--- agent/db.py
+++ agent/db.py
@@ -2,6 +2,7 @@ from pymongo import MongoClient
 from bson import ObjectId
 import os
 from dotenv import load_dotenv
+from datetime import datetime
 
 load_dotenv()
 
@@ -20,3 +21,10 @@ def get_transaction_by_category(user_id:str,category:str):
     user_id=ObjectId(user_id)
     transaction=list(trans_col.find({"user":user_id,"category":category}))
     return transaction
+
+
+def get_monthly_transaction(user_id:str,month:int,year:int):
+    start_date=datetime(year,month,1)
+    end_date=datetime(year,month+1,1)
+    transactions=list(trans_col.find({"createdAt":{"$gte":start_date,"$lte":end_date}}))
+    return transactions
