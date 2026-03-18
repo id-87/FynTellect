956605446764d30ca4c3b67d5c85f296b16e608d
[Harshit Gupta] <hrstgpt87@gmail.com>
2026-03-18 22:20:36 +0530

    Sytax fix

--- agent/db.py
+++ agent/db.py
@@ -13,6 +13,6 @@ trans_col=db["transactions"]
 
 def get_transaction(user_id:str):
     user_id=ObjectId(user_id)
-    transactions=list(trans_col[{"user":user_id}])
+    transactions=list(trans_col.find({"user":user_id}))
     return transactions
 
