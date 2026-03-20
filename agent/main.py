c4a56455f40bf164a6b7c88a8703dcac18b509cd
[Harshit Gupta] <hrstgpt87@gmail.com>
2026-03-20 18:38:20 +0530

    Fast api instance

---
+++ agent/main.py
@@ -0,0 +1,25 @@
+import jwt
+from fastapi import FastAPI
+from pydantic import BaseModel
+import os
+JWT_SECRET=os.getenv("JWT_SECRET")
+def verify_token(token):
+    decoded=jwt.decode(token,JWT_SECRET,algorithms=["HS256"])
+    return decoded
+
+app=FastAPI()
+
+class ChatRequest(BaseModel):
+    message: str
+
+
+@app.get("/health")
+def health():
+    return "Fast api running"
+@app.post('/chat')
+def chat(message):
+    token=message.
+
+
+
+
