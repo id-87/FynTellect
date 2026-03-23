60ea56ebbae1c0648020d47edf68b9e321d9094b
[Harshit Gupta] <hrstgpt87@gmail.com>
2026-03-23 19:58:36 +0530

    transaction handling

--- backend/src/services/transactionService.js
+++ backend/src/services/transactionService.js
@@ -1,4 +1,5 @@
 // const express=require('express')
+
 const Transactions=require('../models/transactionModel')
 async function postTransaction(req,res){
     try{
@@ -30,26 +31,55 @@ async function getAllTransactions(req,res){
     }
 }
 
+async function getById(req,res){
+    try{
+        const transaction_id=req.params.id
+        const resp=await Transactions.find({where:{_id:transaction_id}})
+        return res.status(200).json({status:"success",transactions:resp})
+    }catch(err){
+        return res.status(500).json({
+            message:"Internal Server Error",
+            error:err.message
+        })
+    }
+}
+
+
+async function deleteById(req,res){
+    try{
+        const transaction_id=req.params.id
+        const resp=await Transactions.delete({where:{_id:transaction_id}})
+        return res.status(200).json({status:"Success",message:"Transaction deleted successfully"})
+    }catch(err){
+        return res.status(500).json({message:"Internal server error",error:err.message})
+       }
+}
 
 class TransactionService{
 
     model
     postTrans
     getall
+    getById
+    deleteById
 
     
 
-    constructor(model,post,getall){
+    constructor(model,post,getall,getById,deleteById){
         this.model=model
         this.postTrans=post
         this.getall=getall
+        this.getById=getById
+        this.deleteById=deleteById
 
     }
 }
 
-const tranSer=new TransactionService(Transactions,postTransaction,getAllTransactions)
+const tranSer=new TransactionService(Transactions,postTransaction,getAllTransactions,getById,deleteById)
 
 module.exports={
     postTransaction:tranSer.postTrans,
-    getAllTransactions:tranSer.getall
+    getAllTransactions:tranSer.getall,
+    getById:tranSer.getById,
+    deleteById:tranSer.deleteById
 }
