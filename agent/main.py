f1bc7e2f2bd56a472b12e0f27bf012094cafccfb
[Harshit Gupta] <hrstgpt87@gmail.com>
2026-03-23 12:50:23 +0530

    Fixing env loader

--- agent/main.py
+++ agent/main.py
@@ -3,8 +3,15 @@ from fastapi import FastAPI, Header, HTTPException
 from pydantic import BaseModel
 import os
 from agent import create_agent
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
 # from agent import create_tool
 JWT_SECRET=os.getenv("JWT_SECRET")
+print("JWT SECRET LOADED:", JWT_SECRET) 
+
 def verify_token(token):
     decoded=jwt.decode(token,JWT_SECRET,algorithms=["HS256"])
     return decoded
@@ -18,11 +25,11 @@ class ChatRequest(BaseModel):
 def health():
     return "Fast api running"
 @app.post('/chat')
-def chat(request:ChatRequest,authorisation:str=Header(None)):
-    if not authorisation:
+def chat(request:ChatRequest,authorization:str=Header(None)):
+    if not authorization:
         return {"message":"Forbidden"}
     try:
-        token=authorisation.split(" ")[1]
+        token=authorization.split(" ")[1]
         decoded=verify_token(token)
     except:
         raise HTTPException(status_code=401, detail="Invalid or expired token")
