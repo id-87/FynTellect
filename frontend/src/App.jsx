859008e3f96c3fb14956c3867507dc2d23e29963
[Harshit Gupta] <hrstgpt87@gmail.com>
2026-03-31 16:25:07 +0530

    Wrapping protected routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-
+import ProtectedRoute from './components/ProtectedRoutes'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './global.css'
@@ -20,13 +20,13 @@ function App() {
       <BrowserRouter>
         <Navbar/>
         <Routes>
-          <Route path='/home' element={<Home/>}/>
-          <Route path='/transactions' element={<Transactions/>}/>
-          <Route path='/login' element={<Login/>}/>
-          <Route path='/signup' element={<Signup/>}/>
-          <Route path='/config' element={<Configuration/>}/>
-          <Route path='/alert' element={<Alert/>}/>
-          <Route path='/agent' element={<AgentChat/>}/>
+          <Route path='/home' element={<ProtectedRoute><Home/></ProtectedRoute>}/>
+          <Route path='/transactions' element={<ProtectedRoute><Transactions/></ProtectedRoute>}/>
+          <Route path='/login' element={<ProtectedRoute><Login/></ProtectedRoute>}/>
+          <Route path='/signup' element={<ProtectedRoute><Signup/></ProtectedRoute>}/>
+          <Route path='/config' element={<ProtectedRoute><Configuration/></ProtectedRoute>}/>
+          <Route path='/alert' element={<ProtectedRoute><Alert/></ProtectedRoute>}/>
+          <Route path='/agent' element={<ProtectedRoute><AgentChat/></ProtectedRoute>}/>
         </Routes>
       </BrowserRouter>
     </>
