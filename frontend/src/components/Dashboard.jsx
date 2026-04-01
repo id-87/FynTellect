8ddad87d8ab2c83b42199776296f138212b54e22
[Harshit Gupta] <hrstgpt87@gmail.com>
2026-04-01 18:32:22 +0530

    Planning analytics

--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -3,6 +3,9 @@ import React from 'react'
 const Dashboard = () => {
   return (
     <div>
+        Average spend per department.
+        Max amount spent.
+        does that affect your project.
       
     </div>
   )
